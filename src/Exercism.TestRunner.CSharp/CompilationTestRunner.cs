da41bb5bc3c1c4480c7ffd2a6bed141a5072090b
Erik Schierboom <erik_schierboom@hotmail.com>
2019-12-11 14:01:01 +0100

    Simplify running sequential tests

--- src/Exercism.TestRunner.CSharp/CompilationTestRunner.cs
+++ src/Exercism.TestRunner.CSharp/CompilationTestRunner.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -18,29 +19,31 @@ namespace Exercism.TestRunner.CSharp
         private static readonly TestMessageSink DiagnosticMessageSink = new TestMessageSink();
         private static readonly TestMessageSink ExecutionMessageSink = new TestMessageSink();
 
-        public static async Task<TestRun> Run(Compilation compilation) =>
-            await Run(compilation.UnskipTests().CaptureTracesAsTestOutput().ToAssembly().ToAssemblyInfo());
+        public static async Task<TestRun> Run(Compilation compilation)
+        {
+            var optimizedCompilation = compilation
+                .UnskipTests()
+                .CaptureTracesAsTestOutput();
+
+            return await Run(optimizedCompilation.ToAssembly().ToAssemblyInfo());
+        }
 
         private static async Task<TestRun> Run(IAssemblyInfo assemblyInfo)
         {
-            var unorderedTestResults = new List<TestResult>();
+            var testResults = new List<TestResult>();
 
             ExecutionMessageSink.Execution.TestFailedEvent += args =>
-                unorderedTestResults.Add(TestResult.FromFailed(args.Message));
+                testResults.Add(TestResult.FromFailed(args.Message));
 
             ExecutionMessageSink.Execution.TestPassedEvent += args =>
-                unorderedTestResults.Add(TestResult.FromPassed(args.Message));
+                testResults.Add(TestResult.FromPassed(args.Message));
 
             var testCases = TestCases(assemblyInfo);
 
             using var assemblyRunner = CreateTestAssemblyRunner(testCases, assemblyInfo.ToTestAssembly());
             await assemblyRunner.RunAsync();
 
-            var orderedTestResults = testCases
-                .Select(testCase => unorderedTestResults.First(testResult => testResult.Name == testCase.DisplayName))
-                .ToArray();
-
-            return TestRun.FromTests(orderedTestResults);
+            return TestRun.FromTests(testResults.ToArray());
         }
 
         private static TestAssembly ToTestAssembly(this IAssemblyInfo assemblyInfo) =>
@@ -49,8 +52,8 @@ namespace Exercism.TestRunner.CSharp
         private static IReflectionAssemblyInfo ToAssemblyInfo(this Assembly assembly) =>
             Reflector.Wrap(assembly);
 
-        private static XunitTestAssemblyRunner CreateTestAssemblyRunner(IEnumerable<IXunitTestCase> testCases, TestAssembly testAssembly) =>
-            new XunitTestAssemblyRunner(
+        private static SequentialXunitTestAssemblyRunner CreateTestAssemblyRunner(IEnumerable<IXunitTestCase> testCases, TestAssembly testAssembly) =>
+            new SequentialXunitTestAssemblyRunner(
                 testAssembly,
                 testCases,
                 DiagnosticMessageSink,
@@ -70,10 +73,9 @@ namespace Exercism.TestRunner.CSharp
 
         private static Compilation UnskipTests(this Compilation compilation) =>
             compilation.Rewrite(new UnskipTestsRewriter());
-        
+
         private static Compilation CaptureTracesAsTestOutput(this Compilation compilation) =>
-            compilation
-                .Rewrite(new CaptureTracesAsTestOutputRewriter());
+            compilation.Rewrite(new CaptureTracesAsTestOutputRewriter());
 
         private class UnskipTestsRewriter : CSharpSyntaxRewriter
         {
@@ -100,13 +102,12 @@ namespace Exercism.TestRunner.CSharp
                                 UsingDirective(QualifiedName(
                                 IdentifierName("Xunit").WithLeadingTrivia(Space),
                                 IdentifierName("Abstractions"))))));
-                
+
                 return base.VisitCompilationUnit(node);
             }
 
             private static bool IsTestClass(SyntaxNode descendant) =>
                 descendant is ClassDeclarationSyntax classDeclaration &&
-                classDeclaration.Identifier.Text.EndsWith("Test") &&
                 classDeclaration.Identifier.Text.EndsWith("Test");
 
             public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
@@ -139,9 +140,29 @@ namespace Exercism.TestRunner.CSharp
                                                     IdentifierName("output"))))))
                                 .WithBody(
                                     Block()))));
-                
+
                 return base.VisitClassDeclaration(node);
             }
+        }
+        
+        private class SequentialOrderer : ITestCaseOrderer
+        {
+            public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases) where TTestCase : ITestCase =>
+                testCases.ToList();
+        }
+    
+        private class SequentialXunitTestAssemblyRunner : XunitTestAssemblyRunner
+        {
+            private readonly ITestCaseOrderer _testCaseOrderer = new SequentialOrderer();
+
+            public SequentialXunitTestAssemblyRunner(ITestAssembly testAssembly, IEnumerable<IXunitTestCase> testCases, IMessageSink diagnosticMessageSink, IMessageSink executionMessageSink, ITestFrameworkExecutionOptions executionOptions) : base(testAssembly, testCases, diagnosticMessageSink, executionMessageSink, executionOptions)
+            {
+            }   
+
+            protected override Task<RunSummary> RunTestCollectionAsync(IMessageBus messageBus, ITestCollection testCollection, IEnumerable<IXunitTestCase> testCases,
+                CancellationTokenSource cancellationTokenSource) =>
+                new XunitTestCollectionRunner(testCollection, testCases, DiagnosticMessageSink, messageBus, _testCaseOrderer, new ExceptionAggregator(Aggregator), cancellationTokenSource).RunAsync();
+
         }
     }
 }
