99c337ec8f994eeb5ce73a719bb14da5a6e52325
Erik Schierboom <erik_schierboom@hotmail.com>
2019-12-13 14:25:06 +0100

    Remove unneeded Directory.Build.props

--- src/Exercism.TestRunner.CSharp/ProjectCompiler.cs
+++ src/Exercism.TestRunner.CSharp/ProjectCompiler.cs
@@ -14,38 +14,19 @@ namespace Exercism.TestRunner.CSharp
     {
         public static async Task<Compilation> Compile(Options options)
         {
-            CreateDirectoryBuildPropsFile(options);
-
             var workspace = MSBuildWorkspace.Create();
-            var project = (await workspace.OpenProjectAsync(GetProjectPath(options)))
+            var project = await workspace.OpenProjectAsync(GetProjectPath(options));
+            
+            return await project
                 .AddAdditionalFile("TracingTestBase.cs")
                 .AddAdditionalFile("TestOutputTraceListener.cs")
                 .WithMetadataReferences(GetMetadataReferences())
-                .WithCompilationOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-                
-            var compilation = await project.GetCompilationAsync();
-
-            DeleteDirectoryBuildPropsFile(options);
-
-            return compilation;
+                .WithCompilationOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+                .GetCompilationAsync();
         }
 
         private static string GetProjectPath(Options options) =>
             Path.Combine(options.InputDirectory, $"{options.Slug.Dehumanize().Pascalize()}.csproj");
-
-        private static void CreateDirectoryBuildPropsFile(Options options)
-        {
-            var template = AdditionalFile.Read("Directory.Build.props");
-            var contents = template.Replace("$OutputDirectory", options.OutputDirectory);
-
-            File.WriteAllText(GetDirectoryBuildPropsFilePath(options), contents);
-        }
-
-        private static void DeleteDirectoryBuildPropsFile(Options options) =>
-            File.Delete(GetDirectoryBuildPropsFilePath(options));
-
-        private static string GetDirectoryBuildPropsFilePath(Options options) =>
-            Path.Combine(options.InputDirectory, "Directory.Build.props");
         
         private static Project AddAdditionalFile(this Project project, string fileName) =>
             project.AddDocument(fileName, AdditionalFile.Read(fileName)).Project;
