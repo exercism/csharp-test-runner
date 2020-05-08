b69f3b9945ad672286a91a7eb711929e8848585c
Erik Schierboom <erik_schierboom@hotmail.com>
2020-05-08 11:46:49 +0200

    Use embedded resource to fix path issues

--- src/Exercism.TestRunner.CSharp/AdditionalFile.cs
+++ src/Exercism.TestRunner.CSharp/AdditionalFile.cs
@@ -1,10 +1,16 @@
 using System.IO;
+using System.Reflection;
 
 namespace Exercism.TestRunner.CSharp
 {
     internal static class AdditionalFile
     {
-        public static string Read(string fileName) =>
-            File.ReadAllText(Path.Combine("AdditionalFiles", fileName));
+        public static string Read(string fileName)
+        {
+            var resourceFilePath = $"{typeof(AdditionalFile).Namespace}.AdditionalFiles.{fileName}";
+            using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceFilePath);
+            using var streamReader = new StreamReader(stream);
+            return streamReader.ReadToEnd();
+        }
     }
 }
