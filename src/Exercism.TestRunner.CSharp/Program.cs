64f3ecc57070915ce7c54ba0f483edb2e879e4ab
Erik Schierboom <erik_schierboom@hotmail.com>
2020-06-13 12:10:15 +0200

    Working on version that uses the dotnet CLI

--- src/Exercism.TestRunner.CSharp/Program.cs
+++ src/Exercism.TestRunner.CSharp/Program.cs
@@ -1,6 +1,14 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using CommandLine;
 using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
 using Serilog;
 
 namespace Exercism.TestRunner.CSharp
@@ -22,10 +30,45 @@ namespace Exercism.TestRunner.CSharp
         {
             Log.Information("Running test runner for {Exercise} solution in directory {Directory}", options.Slug, options.InputDirectory);
 
-            var testRun = await TestRunner.Run(options);
-            TestRunWriter.WriteToFile(options, testRun);
+            var projectFilePath = @"/Users/erik/Code/exercism/csharp-test-runner/test/Exercism.TestRunner.CSharp.IntegrationTests/Solutions/MultipleTestsWithTestOutput/Fake.csproj";
+            var projectDirectory = Path.GetDirectoryName(projectFilePath);
+
+            using var workspace = MSBuildWorkspace.Create();
+            var project = await workspace.OpenProjectAsync(projectFilePath);
+
+            var testsDocument = project.Documents.Single(document => document.Name.EndsWith("Test.cs"));
+            var testsRoot = await testsDocument.GetSyntaxRootAsync();
+            testsDocument = testsDocument.WithSyntaxRoot(new UnskipTests().Visit(testsRoot));
+
+            var tryApplyChanges = workspace.TryApplyChanges(testsDocument.Project.Solution);
+
+            var processStartInfo = new ProcessStartInfo("dotnet", "test --verbosity=quiet --logger \"trx;LogFileName=tests.trx\" /flp:v=q");
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.WorkingDirectory = projectDirectory;
+            Process.Start(processStartInfo)?.WaitForExit();
+
+            var logLines = File.ReadLines(Path.Combine(projectDirectory, "msbuild.log"));
+            
+
+
+            // var testRun = await TestRunner.Run(options);
+            // TestRunWriter.WriteToFile(options, testRun);
 
             Log.Information("Ran test runner for {Exercise} solution in directory {Directory}", options.Slug, options.OutputDirectory);
+        }
+    }
+
+    class UnskipTests : CSharpSyntaxRewriter
+    {
+        public override SyntaxNode? VisitAttribute(AttributeSyntax node)
+        {
+            if (node.Name.ToString() == "Fact")
+            {
+                return base.VisitAttribute(node.WithArgumentList(null));
+            }
+
+            return base.VisitAttribute(node);
         }
     }
 }
