b40f4f8562f966939b1e9ef6450b37640e77ee4f
Daniel Gee <dan@dan-gee.co.uk>
2021-09-14 08:24:22 +0100

    Add tests to prove Time Zone information is available in exercise tests

---
+++ test/Exercism.TestRunner.CSharp.IntegrationTests/Solutions/UseTimeZones/FakeTests.cs
@@ -0,0 +1,24 @@
+using Xunit;
+using Exercism.Tests;
+using System.Runtime.InteropServices;
+
+public class FakeTests
+{
+    public static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+    public static string LondonTimeZone => IsWindows ? "GMT Standard Time" : "Europe/London";
+    public static string NewYorkTimeZone => IsWindows ? "Eastern Standard Time" : "America/NewYork";
+    public static string ParisTimeZone => IsWindows ? "W. Europe Standard Time" : "Europe/Paris";
+
+    [Fact]
+    public void Can_Get_TimeZones() => Assert.NotEmpty(Fake.GetTimeZones());
+
+    [Fact]
+    public void Has_London_TimeZone()
+        => Assert.Contains(Fake.GetTimeZones(), tz => tz.Id == LondonTimeZone);
+    [Fact]
+    public void Has_NewYork_TimeZone()
+        => Assert.Contains(Fake.GetTimeZones(), tz => tz.Id == NewYorkTimeZone);
+    [Fact]
+    public void Has_Paris_TimeZone()
+        => Assert.Contains(Fake.GetTimeZones(), tz => tz.Id == ParisTimeZone);
+}
