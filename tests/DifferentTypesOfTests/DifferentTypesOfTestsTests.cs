dda4a9847eee2d94ba60b2a2e2ec2bb4507c7f73
Erik Schierboom <erik_schierboom@hotmail.com>
2025-02-24 12:26:22 +0100

    Disable FsCheck

--- tests/DifferentTypesOfTests/DifferentTypesOfTestsTests.cs
+++ tests/DifferentTypesOfTests/DifferentTypesOfTestsTests.cs
@@ -1,8 +1,8 @@
 using System;
 
 using Xunit;
-using FsCheck.Xunit;
-using FsCheck;
+// using FsCheck.Xunit;
+// using FsCheck;
 
 public class FakeTests
 {
@@ -14,20 +14,20 @@ public class FakeTests
     public void Add_should_add_numbers() =>
         Assert.Equal(2, Fake.Add(1, 1));
     
-    [Theory(Skip = "Remove this Skip property to run this test")]
-    [InlineData(4, 7, 3)]
-    public void Sub_should_subtract_numbers(int expected, int x, int y) =>
-        Assert.Equal(expected, Fake.Sub(x, y));
+    // [Theory(Skip = "Remove this Skip property to run this test")]
+    // [InlineData(4, 7, 3)]
+    // public void Sub_should_subtract_numbers(int expected, int x, int y) =>
+    //     Assert.Equal(expected, Fake.Sub(x, y));
     
-    [CustomPropertyAttribute(Skip = "Remove this Skip property to run this test")]
-    public void Mul_should_multiply_numbers(int x, int y) =>
-        Assert.Equal(x * y, Fake.Mul(x, y));
+    // [CustomPropertyAttribute(Skip = "Remove this Skip property to run this test")]
+    // public void Mul_should_multiply_numbers(int x, int y) =>
+    //     Assert.Equal(x * y, Fake.Mul(x, y));
     
-    [Property(Skip = "Remove this Skip property to run this test")]
-    public Property Div_should_divide_numbers(int x) =>
-        Prop.Throws<DivideByZeroException, int>(new Lazy<int>(() => x / 0));
+    // [Property(Skip = "Remove this Skip property to run this test")]
+    // public Property Div_should_divide_numbers(int x) =>
+    //     Prop.Throws<DivideByZeroException, int>(new Lazy<int>(() => x / 0));
 }
 
-public class CustomPropertyAttribute : PropertyAttribute
-{
-}
+// public class CustomPropertyAttribute : PropertyAttribute
+// {
+// }
