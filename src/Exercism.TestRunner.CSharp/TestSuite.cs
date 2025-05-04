e33ee4a797f31007d3512ab3da8d61b1bc2f4adc
Erik Schierboom <erik_schierboom@hotmail.com>
2025-05-04 18:50:42 +0200

    Allow capturing console and trace output

--- src/Exercism.TestRunner.CSharp/TestSuite.cs
+++ src/Exercism.TestRunner.CSharp/TestSuite.cs
@@ -7,6 +7,8 @@ namespace Exercism.TestRunner.CSharp
 {
     internal class TestSuite
     {
+        private const string AssemblyInfo = "[assembly: CaptureConsole]\n[assembly: CaptureTrace]\n";
+
         private readonly SyntaxTree _originalSyntaxTree;
         private readonly string _originalProjectFile;
         private readonly Options _options;
@@ -20,14 +22,14 @@ namespace Exercism.TestRunner.CSharp
 
         public TestRun Run()
         {
-            Rewrite();
-            RunDotnetTest();
-            UndoRewrite();
+            BeforeTests();
+            RunTests();
+            AfterTests();
 
             return TestRunParser.Parse(_options, _originalSyntaxTree);
         }
 
-        private void RunDotnetTest()
+        private void RunTests()
         {
             var workingDirectory = Path.GetDirectoryName(_options.TestsFilePath)!;
             RunProcess("dotnet", "restore --source /root/.nuget/packages/", workingDirectory);
@@ -46,10 +48,13 @@ namespace Exercism.TestRunner.CSharp
             Process.Start(processStartInfo)?.WaitForExit();
         }
 
-        private void Rewrite()
+        private void BeforeTests()
         {
             RewriteProjectFile();
             RewriteTestsFile();
+            
+            if (CaptureOutput)
+                AddCaptureOuputAssemblyAttributes();
         }
 
         private void RewriteProjectFile() =>
@@ -60,17 +65,28 @@ namespace Exercism.TestRunner.CSharp
                     .Replace("net7.0", "net9.0")
                     .Replace("net8.0", "net9.0"));
 
-        private void RewriteTestsFile() => File.WriteAllText(_options.TestsFilePath, _originalSyntaxTree.Rewrite().ToString());
+        private void RewriteTestsFile() =>
+            File.WriteAllText(_options.TestsFilePath, _originalSyntaxTree.Rewrite().ToString());
+        
+        private void AddCaptureOuputAssemblyAttributes() =>
+            File.WriteAllText(_options.AssemblyInfoFilePath, AssemblyInfo);
 
-        private void UndoRewrite()
+        private void AfterTests()
         {
             UndoRewriteProjectFile();
             UndoRewriteTestsFile();
+            
+            if (CaptureOutput)
+                UndoAddCaptureOuputAssemblyAttributes();
         }
 
         private void UndoRewriteProjectFile() => File.WriteAllText(_options.ProjectFilePath, _originalProjectFile);
 
         private void UndoRewriteTestsFile() => File.WriteAllText(_options.TestsFilePath, _originalSyntaxTree.ToString());
+
+        private void UndoAddCaptureOuputAssemblyAttributes() => File.Delete(_options.AssemblyInfoFilePath);
+
+        private bool CaptureOutput => _originalProjectFile.Contains("xunit.v3");
 
         public static TestSuite FromOptions(Options options)
         {
